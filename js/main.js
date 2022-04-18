5a2af17a4b919ee3b7ceb7928bd50eaf057059b5
Daniel Cho <danielcho07@gmail.com>
2022-04-18 04:36:25 +0000

    Make dropdown show 7 species in-progress. #1

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // requesting API for data and populating scrollbar with 'Species Name' data
 var targetUrl = encodeURIComponent('https://www.fishwatch.gov/api/species');
 var $species = document.getElementById('species');
-// var $speciesH3 = document.querySelector('.species-h3');
+var $speciesH3 = document.querySelector('.species-h3');
 var xhr = new XMLHttpRequest();
 xhr.open('GET', 'https://lfz-cors.herokuapp.com/?url=' + targetUrl);
 xhr.responseType = 'json';
@@ -11,14 +11,18 @@ xhr.addEventListener('load', function () {
   for (let i = 0; i < xhr.response.length; i++) {
     var $option = document.createElement('option');
     $option.textContent = xhr.response[i]['Species Name'];
+    $option.setAttribute('value', xhr.response[i]['Species Name']);
+    $option.setAttribute('class', 'species');
+    $option.setAttribute('id', 'species-name');
     $species.appendChild($option);
   }
 });
 xhr.send();
 
+var $img = document.querySelector('.species-image');
 var $selTabContainer = document.querySelector('.select-tab-container');
 var $tabs = document.querySelectorAll('.tab');
-var $views = document.querySelectorAll('.view.column-full');
+var $views = document.querySelectorAll('.view');
 
 $selTabContainer.addEventListener('click', function (event) {
   if (event.target.matches('.tab')) {
@@ -30,7 +34,7 @@ $selTabContainer.addEventListener('click', function (event) {
       }
     }
     var dataViewVal = event.target.getAttribute('data-view');
-    for (var j = 0; $views.length; j++) {
+    for (var j = 0; j < $views.length; j++) {
       if (dataViewVal === $views[j].getAttribute('data-view')) {
         $views[j].className = 'view column-full';
       } else {
@@ -38,4 +42,35 @@ $selTabContainer.addEventListener('click', function (event) {
       }
     }
   }
+});
+
+var $optionList = document.querySelectorAll('.species');
+var $speciesText = document.querySelector('.species-name-text');
+var $funFactText = document.querySelector('.fun-fact-text');
+
+$species.addEventListener('change', function (event) {
+  if (event.target.matches('.species')) {
+    for (var i = 0; i < $optionList.length; i++) {
+      for (var j = 0; j < $optionList[i].length; j++) {
+        if (event.target.value === $optionList[i][j].value) {
+          // console.log('$optionList[i][j].value:', $optionList[i][j].value);
+          // console.log('$optionList[i][j]:', $optionList[i][j]);
+          // console.log('j:', j);
+          if (xhr.response[j]['Image Gallery'][0].src === null) {
+            $img.setAttribute('src', 'images/placeholder.png');
+          } else {
+            $speciesH3.textContent = xhr.response[j]['Species Name'];
+            $img.setAttribute('src', xhr.response[j]['Image Gallery'][0].src);
+            $img.setAttribute('alt', xhr.response[j]['Image Gallery'][0].alt);
+            $speciesText.textContent = 'Species Name: ' + ' ' + xhr.response[j]['Species Name'];
+            $funFactText.textContent = 'Fun Fact: ' + ' ' + xhr.response[j].Quote;
+          }
+        }
+      }
+    }
+  }
+  // console.log(event.target);
+  // console.log(event.target.value);
+  // console.log($optionList);
+  // console.log($img);
 });
